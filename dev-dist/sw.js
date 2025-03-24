884be70696651eb0ec9483bac6de7b8617d56c01
Lahori Venkatesh <lahorivenkatesh709@gmail.com>
2025-03-24 18:17:42 +0530

    final update code

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-4b45afec'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sqiu3s6mj8g"
+    "revision": "0.qlk00j7pb3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
